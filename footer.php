ab382d380c397224a19805c16a980508708cb8c9
Kordian Szust <kordianszust@gmail.com>
2022-10-26 15:46:34 +0200

    Header and footer cleanup Homepage file setup

--- footer.php
+++ footer.php
@@ -11,23 +11,6 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'felmex-sc' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'felmex-sc' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'felmex-sc' ), 'felmex-sc', '<a href="http://ks-group.pl">Kordian Szust</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
 
 </body>
---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+
+<?php
+get_template_part( 'template-parts/content-homepage' );
+?>
+
+<?php
+get_footer();
