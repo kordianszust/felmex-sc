83e1b226169737b4a84315fcaa44d96fa64f621c
Kordian Szust <kordianszust@gmail.com>
2022-11-19 16:37:04 +0100

    Footer HTML markup

--- footer.php
+++ footer.php
@@ -10,6 +10,111 @@
  */
 
 ?>
+<footer>
+
+    <div class="container container__footer">
+
+        <div class="col">
+            <h4>
+                Pomoc
+            </h4>
+            <ul>
+                <li>
+                    <a href="#">FAQ</a>
+                </li>
+                <li>
+                    <a href="#"> Zwroty i reklamacje</a>
+                </li>
+                <li>
+                    <a href="#"> Koszty i formy dostawy</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col">
+            <h4>
+                Moje konto
+            </h4>
+            <ul>
+                <li>
+                    <a href="#"> Moje konto</a>
+                </li>
+                <li>
+                    <a href="#"> Ustawienia konta</a>
+                </li>
+                <li>
+                    <a href="#"> Przechowalnia</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col">
+            <h4>
+                Płatności i dostawa
+            </h4>
+            <ul>
+                <li>
+                    <a href="#"> Formy płatności</a>
+                </li>
+                <li>
+                    <a href="#"> Czas realizacji</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col">
+            <h4>
+                Informacje
+            </h4>
+            <ul>
+                <li>
+                    <a href="#"> Regulamin</a>
+                </li>
+                <li>
+                    <a href="#"> Zostań naszym partnerem</a>
+                </li>
+                <li>
+                    <a href="#"> Polityka prywatności</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col">
+            <h4>
+                Sklep
+            </h4>
+            <ul>
+                <li>
+                    <a href="#"> O nas</a>
+                </li>
+                <li>
+                    <a href="#"> Blog</a>
+                </li>
+                <li>
+                    <a href="#"> Promocje</a>
+                </li>
+                <li>
+                    <a href="#"> Nowości</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="container-fluid container__footer-p">
+        <p>
+            Czekolada do picia, czekolada do fontann, czekoladziarka, kakao, kawa ziarnista, ekspres do kawy, naturalne
+            herbaty. MARKI: Van Houten, Callebaut, CacaoBarry, Caprimo, Montecelio, Stracto, Cafe Rovi, Lugomer, Ekland,
+            Coffeeta, DecoMorreno, Pastella. Sklep internetowy prowadzony przez Felmex ul. Pojezierska 90A, 91-341 Łódź,
+            NIP 9471946106, REGON 101787861
+        </p>
+    </div>
+    <div class="container-fluid container__footer-bar">
+        <p>
+            sklepczekolada.pl - 2022
+        </p>
+        <p style="float: right">
+            Felmex
+        </p>
+    </div>
+
+
+</footer>
+
 
 <?php wp_footer(); ?>
 
